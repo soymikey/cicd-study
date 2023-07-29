3374ebe34b70ebab43f29b3145f23ffd77b16916
Michael ChengMQ <michael.chengmq@finpoints.com>
2023-07-29 17:16:42 +0800

    feat: add feature-v3.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ const App: FC<Props> = ({ value }) => {
         <p>Hello Vite + React!</p>
         <p>feature-v.1.0.0</p>
         <p>feature-v.2.0.0</p>
+        <p>feature-v.3.0.0</p>
         <p>
           <button role="button" type="button" onClick={() => setCount((v) => v + 1)}>
             count is: {count}
