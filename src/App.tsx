bf5fa9b3c60b1bc1300ed5b0b30ef820f95888e7
Michael ChengMQ <michael.chengmq@finpoints.com>
2023-07-29 18:02:14 +0800

    feat: update e2e:test config2

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ const App: FC<Props> = ({ value }) => {
         <p>feature-v.1.0.0</p>
         <p>feature-v.2.0.0</p>
         <p>feature-v.3.0.0</p>
+        <p>main-更新e2e</p>
         <p>
           <button role="button" type="button" onClick={() => setCount((v) => v + 1)}>
             count is: {count}
